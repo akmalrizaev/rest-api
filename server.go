feededc1a8a1e58aff2a331874be5e5467554102
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-11-16 10:30:31 +0500

    adding HTTP2 and HTTPS

--- server.go
+++ server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 func main() {
@@ -16,13 +19,38 @@ func main() {
 		fmt.Fprintf(w, "Handling users")
 	})
 
-	const port string = ":3000"
+	port := 3000
+
+	// Load the TLS cert and key
+	cert := "cert.pem"
+	key := "key.pem"
+
+	// Configure TLS
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	// Create a custom Server
+	server := &http.Server{
+		Addr:      fmt.Sprintf(":%d", port),
+		Handler:   nil,
+		TLSConfig: tlsConfig,
+	}
+
+	// Enable http2
+	http2.ConfigureServer(server, &http2.Server{})
 
 	fmt.Println("Server Listening on port: ", port)
 
-	err := http.ListenAndServe(port, nil)
+	err := server.ListenAndServeTLS(cert, key)
 	if err != nil {
-		log.Fatalln("error starting server", err)
+		log.Fatalln("Could not start server", err)
 	}
+
+	// HTTP 1.1 Server without TLS
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// if err != nil {
+	// 	log.Fatalln("error starting server", err)
+	// }
 
 }
