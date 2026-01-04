d67cc96bf1da9395f496e76e420a74c73b8e327e
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2026-01-04 09:24:02 +0500

    Connect API to SQL and Environment Variables

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"simpleapi/internal/api/middlewares"
 	"simpleapi/internal/api/router"
+	"simpleapi/internal/repositories/sqlconnect"
+
+	"github.com/joho/godotenv"
 )
 
 type User struct {
@@ -16,16 +20,27 @@ type User struct {
 
 func main() {
 
-	port := ":3000"
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+
+	_, err = sqlconnect.ConnectDB()
+	if err != nil {
+		fmt.Println("Error---:", err)
+		return
+	}
+
+	port := os.Getenv("API_PORT")
 
 	mux := router.Router()
 
 	// rl := middlewares.NewRateLimiter(5, time.Minute)
 
-	fmt.Println("Server is running on port", port)
+	fmt.Println("Server is running on port:", port)
 	// err := http.ListenAndServe(port, rl.Middleware(middlewares.Compression(middlewares.ResponseTimeMiddleware(middlewares.Cors(middlewares.SecurityHeaders(mux))))))
 	// err := http.ListenAndServe(port, ApplyMiddlewares(mux, middlewares.SecurityHeaders, middlewares.Cors, middlewares.ResponseTimeMiddleware, middlewares.Compression, rl.Middleware))
-	err := http.ListenAndServe(port, middlewares.SecurityHeaders(mux))
+	err = http.ListenAndServe(port, middlewares.SecurityHeaders(mux))
 	if err != nil {
 		log.Fatal("Error starting the server", err)
 	}
---
+++ internal/repositories/sqlconnect/sqlconfig.go
@@ -0,0 +1,44 @@
+package sqlconnect
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectDB() (*sql.DB, error) {
+
+	fmt.Println("Trying to connect MariaDB")
+
+	/*
+
+		Environment variables loaded in main function server.go
+
+			err := godotenv.Load()
+			if err != nil {
+				return nil, err
+			}
+
+	*/
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	dbport := os.Getenv("DB_PORT")
+	host := os.Getenv("HOST")
+
+	// connectionString := "root:password@tcp(127.0.0.1:3307)/" + dbname
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, dbport, dbname)
+
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		// panic(err)
+		return nil, err
+	}
+	fmt.Println("Connected to MariaDB")
+
+	return db, nil
+
+}
